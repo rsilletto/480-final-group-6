f57372004bd8b4beb019d0bc545563221c5065ee
Brevin Horton <blh6705@g.rit.edu>
2025-03-31 17:32:46 -0400

    vite updates, part 1 code test

--- final/src/main.js
+++ final/src/main.js
@@ -1,29 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-import * as THREE from 'three'
-import * as LocAR from 'locar'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import * as THREE from 'three';
+import * as LocAR from 'locar';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.001, 100);
@@ -43,8 +19,8 @@ const locar = new LocAR.LocationBased(scene, camera);
 const cam = new LocAR.WebcamRenderer(renderer);
 
 
-locar.startGps();
-locar.add(cube, 43.0839078, -77.6763909);
+locar.fakeGps(-0.72, 51.05);
+locar.add(cube, -0.72, 51.0501);
 
 renderer.setAnimationLoop(animate);
 
